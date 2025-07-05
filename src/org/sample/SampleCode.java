d7883d8d00db92b3233582f400d554638da3a996
Sabasiva1629 <sabasiva1629@gmail.com>
2025-07-05 09:44:59 +0530

    list added

--- src/org/sample/SampleCode.java
+++ src/org/sample/SampleCode.java
@@ -1,8 +1,28 @@
 package org.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SampleCode {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		// create Object for List
+		List<String> li = new ArrayList<String>();
+		li.add("A");
+		li.add("B");
+		li.add("C");
+		li.add("D");
+		li.add("E");
+		li.add("F");
+		// Iterating list
+		for (String string : li) {
+			System.out.println(string);
+		}
+
+		for (int i = 0; i < li.size(); i++) {
+			System.out.println(li.get(i));
+		}
+
 	}
 }
